3bc78a24c93270d754f58715377e22ef5cf96e0f
Karinapaa <129732280+Karinapaa@users.noreply.github.com>
2024-05-18 12:23:08 +0300

    forms.py

--- web_karina_django/app/forms.py
+++ web_karina_django/app/forms.py
@@ -11,7 +11,7 @@ from .models import VideoPost
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class BootstrapAuthenticationForm(AuthenticationForm):
