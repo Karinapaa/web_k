1ec8a7425354a3a6a5117786e216df93d1fc1b57
Karinapaa <129732280+Karinapaa@users.noreply.github.com>
2024-05-18 20:44:27 +0300

    forms.py

--- web_karina_django/app/forms.py
+++ web_karina_django/app/forms.py
@@ -8,7 +8,6 @@ from .models import Blog
 from django import forms
 from .models import VideoPost
 
-
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
